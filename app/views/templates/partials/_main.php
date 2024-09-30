84aba5accce2974e5ab9b5d58cdf748d98ad4e40
francis <sixfranc7@gmail.com>
2024-09-30 19:10:18 +0200

    integration Template + Partial

---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,169 @@
+<div id="main">
+      <div class="container">
+        <div class="row">
+          <!-- About Me (Left Sidebar) Start -->
+          <div class="col-md-3">
+            <div class="about-fixed">
+               
+              <?php include '../app/views/templates/partials/_aside.php'; ?>
+
+              <div class="my-detail">
+                <div class="white-spacing">
+                  <h1>Alex Parker</h1>
+                  <span>Web Developer</span>
+                </div>
+
+                <ul class="social-icon">
+                  <li>
+                    <a href="#" target="_blank" class="facebook"
+                      ><i class="fa fa-facebook"></i
+                    ></a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank" class="twitter"
+                      ><i class="fa fa-twitter"></i
+                    ></a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank" class="linkedin"
+                      ><i class="fa fa-linkedin"></i
+                    ></a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank" class="github"
+                      ><i class="fa fa-github"></i
+                    ></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- About Me (Left Sidebar) End -->
+
+          <!-- Blog Post (Right Sidebar) Start -->
+          <div class="col-md-9">
+            <div class="col-md-12 page-body">
+              <div class="row">
+                <div class="col-md-12 content-page">
+                  <!-- ADD A POST -->
+                  <div>
+                    <a href="form.html" type="button" class="btn btn-primary"
+                      >Add a Post</a
+                    >
+                  </div>
+                  <!-- ADD A POST END -->
+
+                  <!-- Blog Post Start -->
+                  <div class="col-md-12 blog-post row">
+                    <div class="post-title">
+                      <a href="single.html"
+                        ><h1>
+                          How to make your company website based on bootstrap
+                          framework on localhost?
+                        </h1></a
+                      >
+                    </div>
+                    <div class="post-info">
+                      <span>2016-03-03</span> | <span>Life style</span>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Molestiae ut ratione similique temporibus tempora dicta
+                      soluta? Qui hic, voluptatem nemo quo corporis dignissimos
+                      voluptatum debitis cumque fugiat mollitia quasi quod.
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Molestiae ut ratione similique...
+                    </p>
+                    <a
+                      href="single.html"
+                      class="
+                        button button-style button-anim
+                        fa fa-long-arrow-right
+                      "
+                      ><span>Read More</span></a
+                    >
+                  </div>
+                  <!-- Blog Post End -->
+
+                  <!-- Blog Post Start -->
+                  <div class="col-md-12 blog-post">
+                    <div class="post-title">
+                      <a href="single.html"
+                        ><h1>
+                          How to design elegant e-mail newsletter in html for
+                          wish christmas to your subscribers?
+                        </h1></a
+                      >
+                    </div>
+                    <div class="post-info">
+                      <span>2016-02-02</span> | <span>Sport</span>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Molestiae ut ratione similique temporibus tempora dicta
+                      soluta? Qui hic, voluptatem nemo quo corporis dignissimos
+                      voluptatum debitis cumque fugiat mollitia quasi quod.
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Molestiae ut ratione similique...
+                    </p>
+                    <a
+                      href="single.html"
+                      class="
+                        button button-style button-anim
+                        fa fa-long-arrow-right
+                      "
+                      ><span>Read More</span></a
+                    >
+                  </div>
+                  <!-- Blog Post End -->
+
+                  <!-- Blog Post Start -->
+                  <div class="col-md-12 blog-post">
+                    <div class="post-title">
+                      <a href="single.html"
+                        ><h1>
+                          What mistakes every beginner make, when they make
+                          website using bootstrap?
+                        </h1></a
+                      >
+                    </div>
+                    <div class="post-info">
+                      <span>2016-01-01</span> | <span>Life style</span>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Molestiae ut ratione similique temporibus tempora dicta
+                      soluta? Qui hic, voluptatem nemo quo corporis dignissimos
+                      voluptatum debitis cumque fugiat mollitia quasi quod.
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Molestiae ut ratione similique...
+                    </p>
+                    <a
+                      href="single.html"
+                      class="
+                        button button-style button-anim
+                        fa fa-long-arrow-right
+                      "
+                      ><span>Read More</span></a
+                    >
+                  </div>
+                  <!-- Blog Post End -->
+
+                  <?php include '../app/views/templates/partials/_nav.php'; ?>
+
+                </div>
+              </div>
+            </div>
+
+            <!-- Footer Start -->
+            <div class="col-md-12 page-body margin-top-50 footer">
+
+            <?php include '../app/views/templates/partials/_footer.php'; ?> 
+
+            </div>
+            <!-- Footer End -->
+          </div>
+          <!-- Blog Post (Right Sidebar) End -->
+        </div>
+      </div>
+    </div>
